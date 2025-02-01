4f0f84733eb11e2695c213c86de4a4a5e120d135
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-01-31 19:12:45 -0800

    prebuild config changes.

---
+++ app.config.js
@@ -0,0 +1,62 @@
+import 'dotenv/config';
+
+export default {
+  expo: {
+    name: 'meetup',
+    slug: 'meetup',
+    version: '1.0.0',
+    scheme: 'meetup',
+    web: {
+      bundler: 'metro',
+      output: 'static',
+      favicon: './assets/favicon.png',
+    },
+    plugins: [
+      'expo-router',
+      [
+        'expo-image-picker',
+        {
+          photosPermission: 'The app accesses your photos to let you share them with your friends.',
+        },
+      ],
+      [
+        'expo-location',
+        {
+          locationAlwaysAndWhenInUsePermission: 'Allow $(PRODUCT_NAME) to use your location.',
+          locationWhenInUsePermission: 'Show current location on map.',
+        },
+      ],
+      [
+        '@rnmapbox/maps',
+        {
+          RNMapboxMapsDownloadToken: process.env.EXPO_MAPBOX_SECRET_TOKEN,
+          RNMapboxMapsVersion: '11.0.0',
+        },
+      ],
+    ],
+    experiments: {
+      typedRoutes: true,
+      tsconfigPaths: true,
+    },
+    orientation: 'portrait',
+    icon: './assets/icon.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: 'com.ezequielnovoa.meetup',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#ffffff',
+      },
+      package: 'com.ezequielnovoa.meetup',
+    },
+  },
+};
