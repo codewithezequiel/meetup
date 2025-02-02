ef8dd34465c4165530a052cb7cddd0b6772faab2
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2025-02-01 21:31:55 -0800

    Add general Mapbox MapView.

--- app/(tabs)/events/map.tsx
+++ app/(tabs)/events/map.tsx
@@ -1,5 +1,15 @@
 import { View } from 'react-native';
+import Mapbox, { Camera, LocationPuck } from '@rnmapbox/maps';
+
+Mapbox.setAccessToken(process.env.EXPO_PUBLIC_MAPBOX_ACCESS_TOKEN!);
 
 export default function EventsMapView() {
-  return <View className="flex-1 bg-red-400" />;
+  return (
+    <View className="flex-1 bg-red-400">
+      <Mapbox.MapView className="h-full w-full flex-1" style={{ height: '100%' }}>
+        <Camera followZoomLevel={10} followUserLocation />
+        <LocationPuck puckBearingEnabled puckBearing="heading" pulsing={{ isEnabled: true }} />
+      </Mapbox.MapView>
+    </View>
+  );
 }
